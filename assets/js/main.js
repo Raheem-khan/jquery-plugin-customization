206c9e46b450fd307e36c40b56bedce73fa985ff
Rahim <rk.khan71@gmail.com>
2018-09-14 23:59:19 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+		$(".home-page-slides").owlCarousel({
+			
+			
+		});
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
